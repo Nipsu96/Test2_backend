a6b7f133ad6727527421b1891581ea98b96acd61
Niko Lahteinen <niko.lahteinen@tuni.fi>
2022-10-14 10:32:53 +0300

    Print.java:(class):add Javadock

--- src/Print.java
+++ src/Print.java
@@ -1,3 +1,8 @@
+/**
+ * @author      Niko Lahteinen & Iita Lakkaneva
+ * @version     2020.1131                  
+ * @since       1.6                        
+ */
 class Print {
     public void Print( ) {
          System.out.println("Hello World!");
