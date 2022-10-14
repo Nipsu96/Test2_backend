91fa4b755e26246430e962d946607a13904d37f9
Iita Lakkaneva <iita.lakkaneva@tuni.fi>
2022-10-14 10:30:40 +0300

    Helloworld.java: (main): Modify to call Print-class.

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,5 +1,6 @@
 class Helloworld {
     public static void main(String[] args) {
-         System.out.println("Hello World!");
+         Print hello = new Print();
+         hello.print();
     }
 }
