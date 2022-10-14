30c4eb16943392b7d595008b16114aa097ff66e7
Iita Lakkaneva <iita.lakkaneva@tuni.fi>
2022-10-14 10:12:08 +0300

    Helloworld.java: (main): Add javadoc

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -3,6 +3,9 @@
  * @author Niko Lähteinen
  */
 class Helloworld {
+    /**
+     * @param args Command line parameters
+     */
     public static void main(String[] args) {
          System.out.println("Hello World!");
     }
